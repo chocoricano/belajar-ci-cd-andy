42fe08b0413153d265d714b7490139def8a29a22
Andyana Nabila <113494333+chocoricano@users.noreply.github.com>
2026-07-10 11:14:01 +0700

    fourth commit

--- test_app.py
+++ test_app.py
@@ -5,6 +5,6 @@ def test_greet():
     assert greet("Bob") == "Hello, Bob!"
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 100
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
